677b8fe7a4bc88f7b2cadca9e356a6abd43a5230
yk-amarly-20 <y.k.83600260@icloud.com>
2021-07-05 10:28:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# atsumori_management
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,28 @@
+package atsumori.management
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module() {
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
