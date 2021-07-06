73966e48e9a6a304af9eb5e73014b59d019d0dad
yk-amarly-20 <y.k.83600260@icloud.com>
2021-07-07 00:17:34 +0900

    feat: add kotlin sql

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -13,6 +13,18 @@ import kotlinx.css.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.features.*
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.sqlobject.SqlObjectPlugin
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+
+// DB関連オブジェクトのインスタンス化
+var jdbi = Jdbi.create("jdbc:postgresql://db:5432/atsumori_db", "user", "pass")
+    .installPlugin(SqlObjectPlugin())
+
+interface UserName{
+    @SqlQuery("SELECT name from users")
+    fun selectNameList(): List<String>
+}
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -25,7 +37,9 @@ fun Application.module() {
 
     install(Routing) {
         get("/") {
-            call.respond("This is root!")
+            val userName: UserName = jdbi.onDemand(UserName::class.java)
+            val names = userName.selectNameList()
+            call.respond(names)
         }
 
         helloName()
